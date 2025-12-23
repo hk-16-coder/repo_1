8832cd1d397d7b64a8345fe5be98a3703c7e5d3a
hk-16-coder <hamzakothari1978@gmail.com>
2025-12-23 16:39:41 +0530

    Add files via upload

---
+++ assign-1_task2.py
@@ -0,0 +1,5 @@
+f_name = input("Enter your first name : ")
+l_name = input("Enter your last name : ")
+
+full_name = f_name +" "+ l_name
+print(f"Hello, {full_name}! Welcome to the python program .")
